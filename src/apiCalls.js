e561a627b5e8e49958a3432c6785eb3046f980c1
Boone Epstein <booneepstein@gmail.com>
2021-03-09 10:30:05 -0600

    Add post method with logged response

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,14 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const postUrl = urlAndTitle => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(urlAndTitle)
+  })
+      .then(response => console.log(response.json()))
+}
